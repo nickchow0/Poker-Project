62c79e5685563cbe4c8209309df6b1a0a9bc4024
maxlai1 <maximilianlai@hotmail.com>
2016-11-16 22:40:19 -0800

    hihi

--- Hand Evaluation/player.py
+++ Hand Evaluation/player.py
@@ -2,13 +2,13 @@
 # import sys, random
 
 class player:
-	def __init__(self, personality, own_cards, blind, dealer = 0):
+	def __init__(self, personality, own_cards, bigblind, dealer = 0):
 		self.balance = 1000
 		self.personality = personality
 		self.own_cards = [own_cards[0], own_cards[1]]
 		# self.poker = poker
 		self.dealer = dealer
-		self.blind = blind
+		self.bigblind = bigblind
 		# # self.bets = []
 		# if self.dealer != 1:
 		# 	self.bets = [self.blind]
@@ -24,6 +24,17 @@ class player:
 	def is_dealer(self):
 		return self.dealer
 
+	def set_cards(self, cards):
+		self.own_cards = cards
+
+	def add_balance(self, change):
+		self.balance += change
+
+	def set_dealer(self):
+		self.dealer = 1
+
+	def remove_dealer(self):
+		self.dealer = 0
 	# def get_bets(self):
 	# 	return self.bets
 
